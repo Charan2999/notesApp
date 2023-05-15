6d29961851d23e1548bba7f2ec6bdce3eb4d4085
Charan <121006199+Charan2999@users.noreply.github.com>
2023-05-15 11:31:22 +0530

    Add files via upload

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ export default function Header() {
   return (
     <div className="header">
       <h1>Notes</h1>
-      <button className="toggle btn">Toggle mode</button>
+      {/* <button className="toggle btn">Toggle mode</button> */}
     </div>
   );
 }
